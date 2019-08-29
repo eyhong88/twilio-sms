489dda1df07aede0a032492f4b4f8b67eabd3c4f
eyhong88 <eyhong88@gmail.com>
2019-08-29 15:38:25 -0700

    Update SmsSender.java

--- src/main/java/com/example/twiliosms/SmsSender.java
+++ src/main/java/com/example/twiliosms/SmsSender.java
@@ -6,8 +6,8 @@ import com.twilio.type.PhoneNumber;
 
 public class SmsSender {
     // Find your Account Sid and Auth Token at twilio.com/console
-    public static final String ACCOUNT_SID = "AC9690af8b958b876e7248b483b73efe4d";
-    public static final String AUTH_TOKEN = "01eba08b13dff728962163d527e59b73";
+    public static final String ACCOUNT_SID = "-";
+    public static final String AUTH_TOKEN = "-";
 
     public static void main(String[] args) {
         Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
